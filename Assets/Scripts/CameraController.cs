660328a64eed029ebe46fab9da573b134c31468e
unknown <c5tirint@PELIKONE8-PC.wpk.tpu.fi>
2016-09-26 09:31:30 +0300

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private float speed;
+	private Vector3 moveTemporary;
+	private float xDifference;
+	private float yDifference;
+	public float movementLimit;
+
+	public bool marioCamera = true;
+	public bool smoothing = true;
+	public float smoothingSpeed;
+
+	void Start () {
+		speed = GameObject.Find ("Player").GetComponent<MovementScript> ().verticalSpeed;
+	}
+
+	void Update () {
+		if (marioCamera) {
+			xDifference = Mathf.Abs (player.transform.position.x - transform.position.x);
+			yDifference = Mathf.Abs (player.transform.position.y - transform.position.y) * (16f/9f);
+
+			if (xDifference >= movementLimit || yDifference >= movementLimit) {
+				moveTemporary = player.transform.position;
+				moveTemporary.z = -10f;
+
+				transform.position = Vector3.MoveTowards (transform.position, moveTemporary, speed * Time.deltaTime);
+
+			}
+		} else if (!marioCamera && !smoothing) {
+			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+		} else if (!marioCamera && smoothing) {
+			transform.position = Vector3.Lerp (transform.position, new Vector3 (player.transform.position.x, player.transform.position.y, -10f), smoothingSpeed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	private Vector2 mousePosition;
+	public float verticalSpeed;
+	public float strafeSpeed;
+	private Vector3 strafeDirection;
+
+	public bool keyboardMovement1 = false;
+	public bool keyboardMovement2 = false;
+	public bool keyboardMovement3 = false;
+	public bool keyboardMovement4 = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		mouseLook();
+		if (keyboardMovement1) {
+			keyboardMovement ();
+		} else if (keyboardMovement2) {
+			keyboardMovement2nd ();
+		} else if (keyboardMovement3) {
+			keyboardMovement3rd ();
+		} else if (keyboardMovement4) {
+			keyboardMovement4th ();
+		}
+
+	}
+
+	void mouseLook() {
+		Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 direction = Input.mousePosition - position;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+	void keyboardMovement() {
+
+		Vector3 direction = (Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position)).normalized;
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			gameObject.transform.position += direction * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.transform.position += -direction * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			strafeDirection = Quaternion.Euler(0, 0, 90) * direction;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			strafeDirection = Quaternion.Euler(0, 0, -90) * direction;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
+		}
+	}
+
+	void keyboardMovement2nd() {
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		gameObject.transform.position += new Vector3 (horizontal, vertical, gameObject.transform.position.z) * Time.deltaTime * verticalSpeed;
+	}
+
+	void keyboardMovement3rd() {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			gameObject.transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.transform.position += Vector3.down * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			gameObject.transform.position += Vector3.left * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			gameObject.transform.position += Vector3.right * verticalSpeed * Time.deltaTime;
+		}
+	}
+
+	void keyboardMovement4th() {
+		Vector3 direction = (Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position)).normalized;
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			gameObject.transform.position += direction * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			gameObject.transform.position += -direction * verticalSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			strafeDirection = Quaternion.Euler(0, 0, 90) * direction;
+			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			strafeDirection = Quaternion.Euler(0, 0, -90) * direction;
+			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
+		}
+	}
+}
