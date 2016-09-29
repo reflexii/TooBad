f4f9f54ae0197ad43e9138bad3a2ff56ac63a1e5
Timo Rintala <timorintalatr@gmail.com>
2016-09-29 14:55:56 +0300

    Walk animations added for the player character.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -9,6 +9,7 @@ public class MovementScript : MonoBehaviour {
 	private Vector3 strafeDirection;
     public GameObject rotateDirectionObject;
     public float rotationDirectionEuler;
+    public bool moving = false;
     private Animator animator;
 
 	public bool keyboardMovement1 = false;
@@ -26,15 +27,7 @@ public class MovementScript : MonoBehaviour {
 
 		mouseLook();
         animations();
-		if (keyboardMovement1) {
-			keyboardMovement ();
-		} else if (keyboardMovement2) {
-			keyboardMovement2nd ();
-		} else if (keyboardMovement3) {
-			keyboardMovement3rd ();
-		} else if (keyboardMovement4) {
-			keyboardMovement4th ();
-		}
+        keyboardMovement();
 
 	}
 
@@ -49,70 +42,36 @@ public class MovementScript : MonoBehaviour {
 
     void animations() {
         animator.SetFloat("Euler", rotationDirectionEuler);
+        animator.SetBool("Moving", moving);
     }
 
+	
+
+	
+
 	void keyboardMovement() {
-
-		Vector3 direction = (Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position)).normalized;
-
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			gameObject.transform.position += direction * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			gameObject.transform.position += -direction * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			strafeDirection = Quaternion.Euler(0, 0, 90) * direction;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			strafeDirection = Quaternion.Euler(0, 0, -90) * direction;
-		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
-		}
-	}
-
-	void keyboardMovement2nd() {
-		float vertical = Input.GetAxis ("Vertical");
-		float horizontal = Input.GetAxis ("Horizontal");
-
-		gameObject.transform.position += new Vector3 (horizontal, vertical, gameObject.transform.position.z) * Time.deltaTime * verticalSpeed;
-	}
-
-	void keyboardMovement3rd() {
-		if (Input.GetKey(KeyCode.UpArrow)) {
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
 			gameObject.transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
+            moving = true;
 		}
-		if (Input.GetKey(KeyCode.DownArrow)) {
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
 			gameObject.transform.position += Vector3.down * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)) {
+            moving = true;
+        }
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
 			gameObject.transform.position += Vector3.left * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
+            moving = true;
+        }
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
 			gameObject.transform.position += Vector3.right * verticalSpeed * Time.deltaTime;
-		}
-	}
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) ||
+            Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A) ||
+            Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) {
 
-	void keyboardMovement4th() {
-		Vector3 direction = (Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position)).normalized;
-
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			gameObject.transform.position += direction * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			gameObject.transform.position += -direction * verticalSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-			strafeDirection = Quaternion.Euler(0, 0, 90) * direction;
-			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			strafeDirection = Quaternion.Euler(0, 0, -90) * direction;
-			gameObject.transform.position += strafeDirection * strafeSpeed * Time.deltaTime;
-		}
+            moving = false;
+        }
 	}
 }
