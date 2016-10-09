39ffd7802370702413c6cb8dd1db0e12997c1fb1
Timo Rintala <timorintalatr@gmail.com>
2016-10-09 18:21:22 +0300

    -Added new tiles to floor for testing -Modified walls to look a bit more like they are supposed to (needs new graphics though i think) -Fixed player trying to push through walls when colliding with them -Added chasing/attacking script for enemy TODO: add sword animation to the player

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Enemy : Character {
 
+    public Transform player;
+    public Transform raycastOrigin;
+    public bool visibleRaycast = true;
+    public int layerMask = 1 << 9;
+    public float movementSpeed;
+    public float spotDistance;
+
     void Awake()
     {
         transform.tag = "Enemy";
@@ -10,7 +17,7 @@ public class Enemy : Character {
 
 	void Update ()
     {
-	
+        RayCasting();
 	}
 
     void DropLoot()
@@ -22,5 +29,34 @@ public class Enemy : Character {
     {
         DropLoot();
         Destroy(gameObject);
+    }
+
+    public void aggroPlayer() {
+        if (player.gameObject != null && gameObject != null) {
+            Vector3 direction = player.transform.position - gameObject.transform.position;
+            if ((player.transform.position - gameObject.transform.position).magnitude < 1.5f) {
+                Debug.Log("Stopping to attack player");
+                Attack(player.transform.position - gameObject.transform.position);
+            } else {
+                Debug.Log("Chasing!");
+                gameObject.transform.position += direction * movementSpeed * Time.deltaTime;
+            }
+        } 
+    }
+
+    public void RayCasting() {
+        if ((player.transform.position - gameObject.transform.position).magnitude < spotDistance) {
+            //draws a line, if visibleRaycast = true
+            if (visibleRaycast) {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.white);
+            }
+
+            float magnitude = (transform.position - player.transform.position).magnitude;
+            if (!Physics2D.Raycast(transform.position, player.transform.position - transform.position, magnitude, layerMask)) {
+                aggroPlayer();
+            } else {
+                Debug.Log("Wall in the way!");
+            }
+        } 
     }
 }
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -12,11 +12,18 @@ public class MovementScript : MonoBehaviour {
     public bool moving = false;
     private Animator animator;
     private Player player;
+    public int layerMask = 1 << 9;
 
-	public bool keyboardMovement1 = false;
-	public bool keyboardMovement2 = false;
-	public bool keyboardMovement3 = false;
-	public bool keyboardMovement4 = false;
+    //player raycast stuff
+    private float yDif = 0.6f;
+    private float horizontalRayLength = 0.25f;
+    private float verticalRayLength = 0.35f;
+    private float padding = 0.1f;
+
+    public bool rightBlocked = false;
+    public bool leftBlocked = false;
+    public bool topBlocked = false;
+    public bool bottomBlocked = false;
 
 	// Use this for initialization
 	void Start () {
@@ -31,6 +38,7 @@ public class MovementScript : MonoBehaviour {
         animations();
         keyboardMovement();
         updatePlayerDir();
+        checkWalls();
 
 	}
 
@@ -67,19 +75,19 @@ public class MovementScript : MonoBehaviour {
     }
    
 	void keyboardMovement() {
-		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+		if (Input.GetKey(KeyCode.UpArrow) && !topBlocked || Input.GetKey(KeyCode.W) && !topBlocked) {
 			gameObject.transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
             moving = true;
 		}
-		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+		if (Input.GetKey(KeyCode.DownArrow) && !bottomBlocked || Input.GetKey(KeyCode.S) && !bottomBlocked) {
 			gameObject.transform.position += Vector3.down * verticalSpeed * Time.deltaTime;
             moving = true;
         }
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+		if (Input.GetKey(KeyCode.LeftArrow) && !leftBlocked || Input.GetKey(KeyCode.A) && !leftBlocked) {
 			gameObject.transform.position += Vector3.left * verticalSpeed * Time.deltaTime;
             moving = true;
         }
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+		if (Input.GetKey(KeyCode.RightArrow) && !rightBlocked || Input.GetKey(KeyCode.D) && !rightBlocked) {
 			gameObject.transform.position += Vector3.right * verticalSpeed * Time.deltaTime;
             moving = true;
         }
@@ -91,4 +99,43 @@ public class MovementScript : MonoBehaviour {
             moving = false;
         }
 	}
+    
+    void checkWalls() {
+        //visual right check
+        Debug.DrawRay(transform.position - new Vector3(-padding, yDif, 0f), new Vector3(horizontalRayLength, 0f, 0f), Color.yellow);
+        //visual left check
+        Debug.DrawRay(transform.position - new Vector3(padding, yDif, 0f), new Vector3(-horizontalRayLength, 0f, 0f), Color.yellow);
+        //visual top check
+        Debug.DrawRay(transform.position - new Vector3(0f, -padding + yDif, 0f), new Vector3(0f, verticalRayLength, 0f), Color.yellow);
+        //visual bottom check
+        Debug.DrawRay(transform.position - new Vector3(0f, padding + yDif, 0f), new Vector3(0f, -verticalRayLength, 0f), Color.yellow);
+
+        //check right wall
+        if (Physics2D.Raycast(transform.position - new Vector3(-padding, yDif, 0f), Vector3.right, horizontalRayLength, layerMask)) {
+            rightBlocked = true;
+        } else {
+            rightBlocked = false;
+        }
+
+        //check left wall
+        if (Physics2D.Raycast(transform.position - new Vector3(padding, yDif, 0f), Vector3.left, horizontalRayLength, layerMask)) {
+            leftBlocked = true;
+        } else {
+            leftBlocked = false;
+        }
+
+        //check top wall
+        if (Physics2D.Raycast(transform.position - new Vector3(0f, -padding + yDif, 0f), Vector3.up, verticalRayLength, layerMask)) {
+            topBlocked = true;
+        } else {
+            topBlocked = false;
+        }
+
+        //check bottom wall
+        if (Physics2D.Raycast(transform.position - new Vector3(0f, padding + yDif, 0f), Vector3.down, verticalRayLength, layerMask)) {
+            bottomBlocked = true;
+        } else {
+            bottomBlocked = false;
+        }
+    }
 }
