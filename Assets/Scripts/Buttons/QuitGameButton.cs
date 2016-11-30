bd580225c3324000f794e8058fe59cce19bece80
Markus Hyvärinen <hyvarmarku@hotmail.fi>
2016-11-30 15:45:54 +0200

    Added elements for ingame-menu(for restarting, quitting...also pauses the game) and for confirmation box(Are you sure that you want to do...). Those will be used as well when the player dies. Creating scripts for those next.

---
+++ Assets/Scripts/Buttons/QuitGameButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class QuitGameButton : MyButton
+{
+    protected override void OnClickFunction()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/GUI/Dialog/ConfirmationDialog.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfirmationDialog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
