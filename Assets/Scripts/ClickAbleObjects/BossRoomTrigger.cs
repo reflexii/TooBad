328e1e072ff0e45d3f855ff576711303cd664b1a
Timo Rintala <timorintalatr@gmail.com>
2016-12-02 21:54:48 +0200

    -Added music to main menu -Added music to level1 -Added music to boss fight -Added music script for fade ins and fade outs for different parts -Added better lights to boss fight

--- Assets/Scripts/ClickAbleObjects/BossRoomTrigger.cs
+++ Assets/Scripts/ClickAbleObjects/BossRoomTrigger.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BossRoomTrigger : MonoBehaviour
 {
     public InteractiveObject doorToClose;
+    public GameObject musicObject;
+    public GameObject bossLight;
+    public float wantedLightIntensity;
+    private bool lightSeq = false;
+    private bool readyToDestroy = false;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
             doorToClose.gameObject.SetActive(true);
-            GameObject.FindGameObjectWithTag("LightSettings").GetComponent<MasterLightSettings>().TurnLightsOnOrOff(true);
+            //GameObject.FindGameObjectWithTag("LightSettings").GetComponent<MasterLightSettings>().TurnLightsOnOrOff(true);
+            musicObject.GetComponent<MusicScript>().changeToBossMusic();
 
+            startLightSequence();
+            
+        }
+    }
+
+    public void startLightSequence() {
+        lightSeq = true;
+    }
+
+    public void Update() {
+        if (lightSeq) {
+           if (bossLight.GetComponent<Light>().intensity < wantedLightIntensity) {
+                bossLight.GetComponent<Light>().intensity += 2 * Time.deltaTime;
+            } else {
+                readyToDestroy = true;
+            }
+        }
+
+        if (readyToDestroy) {
             Destroy(gameObject);
         }
+
     }
 }
---
+++ Assets/Scripts/Utility/MusicScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+
+    public float targetVolume = 0.3f;
+    private AudioSource audioS;
+    public float fadeInInSeconds = 10f;
+    private float fastFadeInOut = 2f;
+    private float volumeAdded;
+    private bool bossMusicSequence = false;
+
+    public AudioClip ingameMusic;
+    public AudioClip bossMusic;
+
+	// Use this for initialization
+	void Start () {
+        audioS = GetComponent<AudioSource>();
+        volumeAdded = targetVolume / fadeInInSeconds;
+        if (ingameMusic != null) {
+            audioS.clip = ingameMusic;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (audioS.volume < targetVolume && !bossMusicSequence) {
+            fadeInMusic();
+        }
+        if (bossMusicSequence) {
+            if (audioS.volume > 0f) {
+                fadeOutMusic();
+            } else {
+                audioS.clip = bossMusic;
+                audioS.Play();
+                bossMusicSequence = false;
+            }
+        }
+	}
+
+    public void fadeInMusic() {
+        audioS.volume += volumeAdded * Time.deltaTime;
+    }
+
+    public void fadeOutMusic() {
+        audioS.volume -= (targetVolume / fastFadeInOut) * Time.deltaTime;
+    }
+
+    public void changeToBossMusic() {
+        bossMusicSequence = true;
+    }
+}
